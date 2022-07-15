ef76f37fd4edd2a853fee8715b424a40c98138a8
RainYuGG <kopk159@gmail.com>
2022-07-15 20:36:43 +0800

    update script

--- script.sh
+++ script.sh
@@ -1,8 +1,7 @@
 cd tvm
 git checkout 395e91ff54543864a90240d18c8efd8c277c758b
 patch -p1 < ../conda-tvm-env/tvm.patch
-#conda env create --file ../conda-tvm-env/build-environment.yaml
-conda env create --file ../conda-tvm-env/local-build-environment.yaml
+conda env create --file ../conda-tvm-env/build-environment.yaml
 conda activate tvm-env
 mkdir build
 cd build
