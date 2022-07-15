9999c316ad839e49f5007dc0581e1a1ff76455ea
RainYuGG <31921037+RainYuGG@users.noreply.github.com>
2022-07-15 20:28:01 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,6 @@
 cd tvm
 git checkout 395e91ff54543864a90240d18c8efd8c277c758b
 patch -p1 < ../conda-tvm-env/tvm.patch
-#cp ../conda-tvm-env/build-environment.yaml conda/.
-#cp ../conda-tvm-env/meta.yaml conda/recipe/.
-#conda env create --file conda/build-environment.yaml
 #conda env create --file ../conda-tvm-env/build-environment.yaml
 conda env create --file ../conda-tvm-env/local-build-environment.yaml
 conda activate tvm-env
